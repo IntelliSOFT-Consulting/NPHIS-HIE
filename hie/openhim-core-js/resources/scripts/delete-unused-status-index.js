aa39e54b5058df943b42796ff19265f276073d2b
Brian Amolo <railamolo@gmail.com>
2024-05-22 16:30:51 +0300

    Migrate to generic OpenHIM Corre

--- hie/openhim-core-js/resources/scripts/delete-unused-status-index.js
+++
@@ -1,5 +0,0 @@
-/* global db */
-
-db.transactions.dropIndex('status_1')
-
-db.transactions.dropIndex('channelID_1')
--- hie/openhim-core-js/test/resources/replica-set-test/setup.sh
+++
@@ -1,1 +0,0 @@
-(cd ./test/resources/replica-set-test ; docker-compose build && docker-compose up -d)
