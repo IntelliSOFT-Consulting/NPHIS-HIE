405e6930b206a5bbfbd3f8b6a26c82f08b4a9c76
nyatindopatrick <nyatindopatrick@gmail.com>
2024-06-19 07:47:34 +0300

    Add patient_update_date to Hive query

--- reports/hive.py
+++ reports/hive.py
@@ -6,14 +6,17 @@ import pg as db
 
 load_dotenv()
 
+
 def get_hive_connection():
-    host = os.getenv('HIVE_SERVER')
-    port = int(os.getenv('HIVE_PORT'))
-    database = 'default'
+    host = os.getenv("HIVE_SERVER")
+    port = int(os.getenv("HIVE_PORT"))
+    database = "default"
     return hive.connect(host=host, port=port, database=database)
 
+
 def get_query():
-    return ''.join([str(item) for item in sql])
+    return "".join([str(item) for item in sql])
+
 
 def query_data():
     print("Querying data from Hive")
