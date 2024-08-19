93e66c10ce3e31342f4c9b1d88d6ef51ac0117e4
itskiprotich <keeprawteachjapheth@gmail.com>
2024-08-19 10:16:20 +0300

    UPD: certificate QR code validation

--- hie/ddcc-chanjo-mediators/src/routes/digital-certificate.ts
+++ hie/ddcc-chanjo-mediators/src/routes/digital-certificate.ts
@@ -15,7 +15,12 @@ import {
   generatePDF,
   savePDFToFileSystem,
 } from "../lib/generatePDF";
-import { vaccineCodes, nonRoutineVaccince } from "../lib/vaccineCodes";
+import {
+  vaccineCodes,
+  nonRoutineVaccince,
+  routineRefinedVaccines,
+  nonRoutineRefinedVaccines,
+} from "../lib/vaccineCodes";
 
 export const router = express.Router();
 router.use(express.json());
@@ -115,7 +120,8 @@ router.post("/", async (req, res) => {
       patientId,
       locationId,
       pdfFile,
-      vaccineCode
+      vaccineCode,
+      docRefId
     );
 
     // create Document/Bundle to attach to DocumentRef above
@@ -190,7 +196,12 @@ router.post(
       let vaccineName = _vaccineCodes[vaccineCode];
       //Loop through
       const immunizations = [];
-      Object.entries(_vaccineCodes).forEach(async ([code, display]) => {
+      console.log("routine starts here *****");
+      console.log(routineRefinedVaccines);
+      console.log("non routine starts here *****");
+      console.log(nonRoutineRefinedVaccines);
+
+      routineRefinedVaccines.forEach(async (vaccine) => {
         const immunization = {
           resourceType: "Immunization",
           id: uuid(), // Generating a unique ID for each immunization
@@ -203,14 +214,14 @@ router.post(
           vaccineCode: {
             coding: [
               {
-                code: code,
-                display: `IMPO-${display}`, // Adjust the display name as needed
+                code: vaccine.nhddCode,
+                display: vaccine.vaccineCode, // Adjust the display name as needed
               },
             ],
-            text: display,
+            text: vaccine.vaccineName,
           },
           patient: {
-            reference: "Patient/d1c0ca77-d466-4984-ba1f-12a8271c9e0b", // Replace with actual patient reference
+            reference: "Patient/7fc8e9ed-09e2-4d7d-949f-3f554c7c61b2", // Replace with actual patient reference
           },
           occurrenceDateTime: new Date().toISOString(),
           recorded: new Date().toISOString(),
@@ -219,7 +230,7 @@ router.post(
           },
           lotNumber: "HDKKD8777847", // Example lot number; replace with actual value if needed
           doseQuantity: {
-            value: 0,
+            value: vaccine.doseNumber,
             unit: "0",
             system: "http://unitsofmeasure.org",
           },
@@ -258,10 +269,7 @@ router.post(
         } catch (error) {
           console.error("Error making POST request:", error);
         }
-
-        immunizations.push(immunization);
       });
-
       console.log("successfully created all the immunizations ****");
     } catch (error) {
       response.statusCode = 400;
@@ -283,186 +291,41 @@ router.post(
     }
   }
 );
-router.post("/nice", async (req, res) => {
-  try {
-    let data = req.body;
-    let patientId = data?.subject?.reference ?? data?.patient?.reference;
-    let immunizationId = data.id;
-    let vaccineCode = data?.vaccineCode?.coding[0]?.code;
-    let vaccineName = _vaccineCodes[vaccineCode];
-
-    // // get all composition resources for this patient & vaccine code  - avoid regeneration of resources
-    // let compositions = await (await FhirApi({ url: `/Composition?subject=${patientId}&type:code=${vaccineCode}` })).data;
-    // if (compositions?.entry) {
-    //     compositions = compositions.entry.map((i: any) => {
-    //         return i.resource?.section?.[0]?.entry?.[0]?.reference.split('/')[1];
-    //     });
-    //     if (compositions.indexOf(immunizationId) > -1) {
-    //         let vaccineFolder = await getVaccineFolder(patientId, vaccineCode);
-    //         let docRefs = await (await FhirApi({
-    //             url: `/DocumentReference?_profile=StructureDefinition/DigitalCertificateDocumentReference&subject=${patientId}`,
-    //             headers:{"Cache-Control":"no-cache"}})).data;
-    //         let previousImmunizations = docRefs?.entry?.map((i: any) => {
-    //             return { item: { reference: `${i?.resource?.resourceType}/${i?.resource?.id}` } }
-    //         }) ?? [];
-    //         // console.log("docRefs:", previousImmunizations);
-    //         let updatedFolder = await (await FhirApi({ url: `/List/${vaccineFolder.id}`, method: "PUT", data: JSON.stringify({ ...vaccineFolder, entry: previousImmunizations }) })).data;
-    //         console.log(updatedFolder);
-    //         res.statusCode = 200;
-    //         res.json({
-    //             "resourceType": "OperationOutcome",
-    //             "id": "certificate-already-exists",
-    //             "issue": [{
-    //                 "severity": "information",
-    //                 "code": "certificate-already-exists",
-    //                 "details": {
-    //                     "text": String("Certificate was already generated")
-    //                 }
-    //             }]
-    //         });
-    //         return;
-    //     }
-    // }
-
-    if (!vaccineName) {
-      res.statusCode = 400;
-      res.json({
-        resourceType: "OperationOutcome",
-        id: "exception",
-        issue: [
-          {
-            severity: "error",
-            code: "exception",
-            details: { text: String("Invalid vaccine code provided") },
-          },
-        ],
-      });
-      return;
-    }
-    console.log(vaccineCode, vaccineName);
-    patientId = String(patientId).split("/")[1];
-    let patient = await (await FhirApi({ url: `/Patient/${patientId}` })).data;
-    if (patient.resourceType === "OperationOutcome") {
-      res.statusCode = 400;
-      res.json(patient);
-      return;
-    }
-
-    // begin processing cert workflow
-    let locationId = data?.location?.reference.split("/")[1];
-    let location = (await FhirApi({ url: `/Location/${locationId}` })).data;
-
-    // get/create vaccine folder and add a new document reference for this immunization
-    let vaccineFolder = await getVaccineFolder(patientId, vaccineCode);
-    console.log(vaccineFolder);
-
-    let docRefId = uuid();
-
-    // create certificate PDF
-    let pdfFile = await generatePDF(vaccineCode, patient, docRefId);
-    if (!pdfFile) {
-      res.json({
-        resourceType: "OperationOutcome",
-        id: "exception",
-        issue: [
-          {
-            severity: "error",
-            code: "exception",
-            details: {
-              text: String("PDF generation failed"),
-            },
-          },
-        ],
-      });
-      return;
-    }
-    savePDFToFileSystem(
-      pdfFile,
-      `${patientId}-${vaccineName}.pdf`.replace("/", "-")
-    );
-
-    // save pdf image to FHIR Server
-    let docRefQR = await createDocumentRefQR(
-      patientId,
-      locationId,
-      pdfFile,
-      vaccineCode
-    );
-
-    // create Document/Bundle to attach to DocumentRef above
-    let composition = await createComposition(data.id);
-    let organization = await createOrganization(location);
-    let document = await createDocument(
-      composition,
-      patient,
-      organization,
-      docRefQR
-    );
-    let docRef = await createDocumentRef(patientId, document.id, vaccineCode);
-
-    // let binaryId  = await createBinary(pdfFile);
-    // console.log(binaryId)
-
-    // update folder - fetch all documentReferences and attach here
-    let docRefs = await (
-      await FhirApi({
-        url: `/DocumentReference?_profile=StructureDefinition/DigitalCertificateDocumentReference&subject=${patientId}&type:code=${vaccineCode}`,
-        headers: { "Cache-Control": "no-cache" },
-      })
-    ).data;
-    // let previousImmunizations = [];
-    let previousImmunizations =
-      docRefs?.entry?.map((i: any) => {
-        return {
-          item: {
-            reference: `${i?.resource?.resourceType}/${i?.resource?.id}`,
-          },
-        };
-      }) ?? [];
-    console.log(previousImmunizations);
-    let updatedFolder = await (
-      await FhirApi({
-        url: `/List/${vaccineFolder.id}`,
-        method: "PUT",
-        data: JSON.stringify({
-          ...vaccineFolder,
-          entry: previousImmunizations,
-        }),
-      })
-    ).data;
-    console.log(updatedFolder);
-    res.statusCode = 200;
-    res.json(updatedFolder);
-    return;
-  } catch (error) {
-    res.statusCode = 400;
-    console.log(error);
-    res.json({
-      resourceType: "OperationOutcome",
-      id: "exception",
-      issue: [
-        {
-          severity: "error",
-          code: "exception",
-          details: {
-            text: JSON.stringify(error),
-          },
-        },
-      ],
-    });
-    return;
-  }
-});
 
 // id maps to a FHIR List / Folder
-router.get("/:id/$validate", async (req, res) => {
+router.get("/validate/:id", async (req, res) => {
   try {
-    let data = req.body;
+    const id = req.params.id;
 
-    // check headers or param for required response type.
-
-    res.statusCode = 400;
-    // res.json(patient);
+    //Pull FHIR Document
+    const certificate = await FhirApi({
+      url: `/DocumentReference/${id}`,
+      headers: { "Cache-Control": "no-cache" },
+    });
+    console.log(certificate);
+    if (!certificate) {
+      res.statusCode = 404;
+      res.json({
+        resourceType: "OperationOutcome",
+        id: "exception",
+        issue: [
+          {
+            severity: "error",
+            code: "exception",
+            details: { text: String("Invalid Certificate ID provided") },
+          },
+        ],
+      });
+      return;
+    }
+    const data =certificate.data; 
+    const base64Data = data?.content[0].attachment.data; 
+    // Decode the Base64 string
+    const decodedData = Buffer.from(base64Data, "base64");
+    console.log("Decoded Data:", decodedData);
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Disposition", `inline; filename="certificate.pdf"`);
+    res.send(decodedData);
     return;
   } catch (error) {
     res.statusCode = 400;
