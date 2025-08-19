4a2c71ce7f7026112b74cfd7b21329b6ef5a0f42
Amolo <railamolo@gmail.com>
2025-08-19 16:47:16 +0000

    Minimal Setup

--- fhir-data-pipes/bunsen/bunsen-core-stu3/src/main/java/com/cerner/bunsen/definitions/stu3/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Support for translating FHIR STU3 resources. Most users won't use this directly, but rather use
- * the bunsen-spark API or similar libraries.
- */
-package com.cerner.bunsen.definitions.stu3;
--- fhir-data-pipes/bunsen/bunsen-core/src/main/java/com/cerner/bunsen/definitions/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Support for traversing FHIR definitions to convert them into other data models. Most users will
- * not use this package, but rather directly use Bunsen's Spark support.
- */
-package com.cerner.bunsen.definitions;
